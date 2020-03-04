2d52d5336271adbc1d4a9eb51144ac4691021c76
hiagor <hiagornew@gmail.com>
2020-03-04 18:18:17 -0300

    receitas

---
+++ Assets/Scripts/ReceitaObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Receita", menuName = "Receita")]
+public class ReceitaObject : ScriptableObject
+{
+    public int Id;
+    public enum RECEITATYPE { POCAO, PEDRA, RUNA, ERVA };
+    public string name;
+    public Sprite imageReceita;
+    public Color corItem;
+    public RECEITATYPE tipoReceita;
+    public float valorVenda;
+    public int qualidadeReceita;
+    public Sprite imageMask;
+
+    [TextArea]
+    public string Descricao;
+}
